bd02678bb9db8b7581c8eb003855af9acfcf038a
Arun Mohan <arun@getrealhealth.com>
2018-06-12 15:14:50 -0400

    initial commit

---
+++ AMLabWeb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AMLabWeb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ AMLabWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AMLabWeb.MvcApplication" Language="C#" %>
